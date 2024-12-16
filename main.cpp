fa8e33d7b45ea7edb2d16044d35554db9fbd2802
Your name <you@example.com>
2024-12-15 18:42:02 -0700

    notes.txt

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 
 // g++ -s core.cpp main.cpp clean-encoding.cpp run_command.cpp -o test -lssl -lcrypto -lpthread -I/home/host/LuaDropper/implant/json/include -I/home/host/LuaDropper/implant/base64/include
 
-// g++ -s core.cpp main.cpp clean-encoding.cpp run_command.cpp ssh_persistence.cpp crontab_persistence.cpp -o test -lssl -lcrypto -lpthread -I./json/include -I./base64/include
+// g++ -s core.cpp main.cpp clean-encoding.cpp run_command.cpp ssh_persistence.cpp crontab_persistence.cpp grab_read_access_token.cpp -o test -lssl -lcrypto -lpthread -I./json/include -I./base64/include
 
 // ldd application.elf
 
